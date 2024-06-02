b6e4d9af1a7820cc12e9a857d8314a9c476765ee
kalaboki <ilia.k@syncspider.com>
2024-06-02 14:21:48 +0400

    refactor iframe code

--- client/src/components/music/Music.vue
+++ client/src/components/music/Music.vue
@@ -1,8 +1,24 @@
 <script lang="ts" setup>
-import { PropType, ref, watchEffect } from "vue";
+import { PropType, ref } from "vue";
 import { Icon } from "@iconify/vue";
-import Slider from "@/components/slider/Slider.vue";
 import { Socket } from "socket.io-client";
+import { player } from "../../composables/music/use-youtube-iframe.ts";
+import Slider from "../slider/Slider.vue";
+
+const {
+  addToQueue,
+  play,
+  pause,
+  playNext,
+  playPrevious,
+  // queue,
+  playState,
+  currentVideo,
+  currentTime,
+  jumpTo,
+} = player;
+
+const videoUrl = ref("");
 
 const props = defineProps({
   socket: {
@@ -11,117 +27,22 @@ const props = defineProps({
   },
 });
 
-const video = ref("");
-
 const showControls = ref(false);
 
-function onPlayerReady(event: any) {
-  // event.target.playVideo();
-}
-
-const time = ref(0);
-const duration = ref(213);
-const name = ref("Give You Up");
-
-let interval: any;
-
-// function onPlayerStateChange(event: any) {
-//   if (event.data === window.YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-
-const player = ref<any>(null);
-
-const playerState = ref<any>(false);
-
-// @ts-ignore
-
-window["onYouTubeIframeAPIReady"] = function onYouTubeIframeAPIReady() {
-  player.value = new window.YT.Player("player", {
-    height: "390",
-    width: "640",
-    videoId: "dQw4w9WgXcQ",
-    playerVars: {
-      playsinline: 1,
-    },
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange,
-    },
-  });
-};
-
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.ENDED) {
-    // Video or music has ended
-    console.log("Music ended");
-    // You can do something here, like play the next track
+function handleSubmit() {
+  if (!videoUrl.value) {
+    return;
   }
+  addToQueue(videoUrl.value);
+  videoUrl.value = "";
 }
-
-function setCurrentTime() {
-  time.value = player.value.getCurrentTime();
-}
-
-function playVideo() {
-  player.value?.playVideo();
-  playerState.value = true;
-  setTimeout(() => {
-    duration.value = player.value.getDuration();
-    name.value = player.value.getVideoData().title;
-  }, 600);
-
-  interval = setInterval(setCurrentTime, 1000);
-}
-
-props.socket.on("music", (videoId: string) => {
-  player.value.loadVideoById(videoId);
-  playVideo();
-});
-
-function handleVideoLoad(e: Event) {
-  if (!video.value) return;
-  e.preventDefault();
-  const videoId = video.value.split("v=")[1].split("&")[0];
-  player.value.loadVideoById(videoId);
-  video.value = "";
-  playVideo();
-  props.socket.emit("music", videoId);
-}
-
-function stopVideo() {
-  player.value.pauseVideo();
-  playerState.value = false;
-  if (interval) clearInterval(interval);
-}
-
-function handleSlide(value: number) {
-  player.value.seekTo(value);
-}
-
-watchEffect((onInvalidate) => {
-  if (showControls.value === true) {
-    const changeState = () => (showControls.value = false);
-
-    document.addEventListener("click", changeState);
-    onInvalidate(() => {
-      document.removeEventListener("click", changeState);
-      console.log("removed");
-    });
-  }
-});
 </script>
 
 <template>
   <div @click.stop>
-    <div class="hidden">
-      <div id="player"></div>
-    </div>
     <div class="relative">
       <Icon
-        :class="playerState ? 'text-red-600' : 'text-blue-400'"
+        :class="playState === 'playing' ? 'text-red-600' : 'text-blue-400'"
         class="cursor-pointer w-6 h-6"
         icon="simple-icons:youtubemusic"
         @click="showControls = !showControls"
@@ -133,40 +54,42 @@ watchEffect((onInvalidate) => {
         <p
           class="my-2 overflow-clip line-clamp-1 text-center text-sm font-medium text-blue-50"
         >
-          {{ name }}
+          {{ currentVideo.title }}
         </p>
         <div class="flex justify-between items-center">
           <Icon
             class="cursor-pointer text-gray-400 w-5 h-5"
             icon="mage:previous-fill"
+            @click="playPrevious"
           ></Icon>
           <Icon
-            v-if="!playerState"
+            v-if="playState !== 'playing'"
             class="cursor-pointer text-gray-400 w-5 h-5"
             icon="mage:play-fill"
-            @click="playVideo"
+            @click="play()"
           ></Icon>
           <Icon
             v-else
             class="cursor-pointer text-gray-400 w-5 h-5"
             icon="mage:pause-fill"
-            @click="stopVideo"
+            @click="pause"
           ></Icon>
           <Icon
             class="cursor-pointer text-gray-400 w-5 h-5"
             icon="mage:next-fill"
+            @click="playNext"
           ></Icon>
         </div>
         <Slider
-          :current="time"
-          :total="duration"
+          :current="currentTime"
+          :total="currentVideo.duration || 0"
           type="time"
-          @update:value="handleSlide"
+          @update:value="jumpTo"
         ></Slider>
 
-        <Form class="mt-2" @submit="handleVideoLoad">
+        <form class="mt-2" @submit.prevent="handleSubmit">
           <input
-            v-model="video"
+            v-model="videoUrl"
             class="w-full bg-transparent border border-gray-500 outline-none rounded text-xs text-gray-500 p-1"
             placeholder="YouTube video URL"
             type="text"
@@ -179,7 +102,7 @@ watchEffect((onInvalidate) => {
               play
             </button>
           </div>
-        </Form>
+        </form>
       </div>
     </div>
   </div>
