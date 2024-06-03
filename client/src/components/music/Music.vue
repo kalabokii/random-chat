8768bd40f3728c57b2778dcbd2eab1ce2fd08519
kalaboki <iliakharkheli3@gmail.com>
2024-06-03 22:08:14 +0400

    add thumbnail and que item display

--- client/src/components/music/Music.vue
+++ client/src/components/music/Music.vue
@@ -4,6 +4,7 @@ import { Icon } from "@iconify/vue";
 import { Socket } from "socket.io-client";
 import Slider from "../slider/Slider.vue";
 import useMiddleMan, { type Video } from "./hooks/use-middle-man.ts";
+import { formatTime } from "@lib/utils/functions.ts";
 
 const videoUrl = ref("");
 
@@ -44,7 +45,7 @@ function handleSubmit() {
 
 <template>
   <div @click.stop>
-    <div class="relative">
+    <div class="relative mr-3">
       <Icon
         :class="playState === 'playing' ? 'text-red-600' : 'text-blue-400'"
         class="cursor-pointer w-6 h-6"
@@ -53,8 +54,16 @@ function handleSubmit() {
       ></Icon>
       <div
         v-if="showControls"
-        class="md:-translate-x-[calc(50%-16px)] z-50 -translate-x-[calc(100%-16px)] bg-gray-800 w-48 left-0 -bottom-2 translate-y-full py-2 px-4 rounded border border-gray-700 absolute"
+        class="overflow-auto h-[25rem] md:-translate-x-[calc(50%-16px)] z-50 -translate-x-[calc(100%-16px)] bg-gray-800 w-64 left-0 -bottom-2 translate-y-full py-2 px-4 rounded border border-gray-700 absolute"
       >
+        <div class="w-full p-2">
+          <img
+            v-if="nowPlaying?.thumbnail"
+            :src="nowPlaying.thumbnail"
+            alt="thumbnail"
+            class="aspect-square object-cover w-full rounded"
+          />
+        </div>
         <p
           class="my-2 overflow-clip line-clamp-1 text-center text-sm font-medium text-blue-50"
         >
@@ -112,6 +121,40 @@ function handleSubmit() {
             </button>
           </div>
         </form>
+
+        <div class="flex-col flex gap-4 mt-4">
+          <div
+            v-for="item of queue"
+            :key="item.id"
+            class="flex items-center gap-2"
+          >
+            <img
+              :src="item.thumbnail"
+              alt="thumbnail"
+              class="shrink-0 aspect-square w-10 h-10 object-cover"
+            />
+            <div class="flex-col items-center justify-center">
+              <p class="text-xs line-clamp-1">{{ item.title }}</p>
+              <p class="text-xs text-gray-500">
+                {{ formatTime(item.duration) }}
+              </p>
+            </div>
+            <div class="grow flex justify-end items-center">
+              <Icon
+                v-if="item.id === currentVideo && playState === 'playing'"
+                class="cursor-pointer text-gray-400 w-5 h-5"
+                icon="mage:pause-fill"
+                @click="pause"
+              ></Icon>
+              <Icon
+                v-else
+                class="cursor-pointer text-gray-400 w-5 h-5"
+                icon="mage:play-fill"
+                @click="play(item.id)"
+              ></Icon>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
