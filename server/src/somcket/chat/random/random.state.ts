c2625319633890dd0440f5529df189063825bba5
kalaboki <iliakharkheli3@gmail.com>
2024-06-05 20:34:24 +0400

    add state interface

--- server/src/somcket/chat/random/random.state.ts
+++ server/src/somcket/chat/random/random.state.ts
@@ -1,10 +1,20 @@
 import { User } from "../types/user.model";
 
+// ? This file is responsible for the state of the random chat
+// ? It will keep track of all users and their status
+
 interface UserCollection {
   [id: string]: User;
 }
 
-function state() {
+export interface RandomState {
+  users: UserCollection;
+  addUser: (user: User) => void;
+  removeUser: (id: string) => void;
+  findFriend: (id: string) => string | undefined;
+}
+
+function state(): RandomState {
   const users: UserCollection = {};
   const queue: Set<string> = new Set();
 
@@ -30,7 +40,7 @@ function state() {
   }
 
   return {
-    users,
+    users: { ...users },
     addUser,
     removeUser,
     findFriend,
